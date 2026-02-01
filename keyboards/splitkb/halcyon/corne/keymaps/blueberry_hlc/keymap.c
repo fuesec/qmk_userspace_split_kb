e0a4c11aa4012d9a0ad5202884ac33abe75ede5b
Christopher Füseschi <christopher.fueseschi@4screen.com>
2026-02-01 18:03:02 +0100

    remove record processing

--- keyboards/splitkb/halcyon/corne/keymaps/blueberry_hlc/keymap.c
+++ keyboards/splitkb/halcyon/corne/keymaps/blueberry_hlc/keymap.c
@@ -184,7 +184,7 @@ oneshot_state os_ctrl_state = os_up_unqueued;
 oneshot_state os_alt_state = os_up_unqueued;
 oneshot_state os_cmd_state = os_up_unqueued;
 
-#ifdef
+#if
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #ifdef CONSOLE_ENABLE
     uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
