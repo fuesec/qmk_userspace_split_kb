1f8f146b50da959858c9e1d9da5d7bdcf5378183
Christopher Füseschi <christopher.fueseschi@4screen.com>
2026-02-03 17:52:02 +0100

    add tapping term for trackpad

--- keyboards/splitkb/halcyon/corne/keymaps/blueberry_hlc/config.h
+++ keyboards/splitkb/halcyon/corne/keymaps/blueberry_hlc/config.h
@@ -24,3 +24,5 @@
 
 #define CAPS_WORD_IDLE_TIMEOUT 3000  // 3 seconds.
 #define HSS(report) host_system_send(record->event.pressed ? report : 0); return false
+
+#define CIRQUE_PINNACLE_TAPPING_TERM 270
