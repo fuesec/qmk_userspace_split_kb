70beb2660f4928500fbb90c105fb7ec96c779cab
Christopher Füseschi <christopher.fueseschi@4screen.com>
2026-02-03 19:46:44 +0100

    changes

--- keyboards/splitkb/halcyon/corne/keymaps/blueberry_hlc/config.h
+++ keyboards/splitkb/halcyon/corne/keymaps/blueberry_hlc/config.h
@@ -25,4 +25,5 @@
 #define CAPS_WORD_IDLE_TIMEOUT 3000  // 3 seconds.
 #define HSS(report) host_system_send(record->event.pressed ? report : 0); return false
 
-#define CIRQUE_PINNACLE_TAPPING_TERM 500
+#define CIRQUE_PINNACLE_TAPPING_TERM 250
+#define CIRQUE_PINNACLE_TOUCH_DEBOUNCE 2000
